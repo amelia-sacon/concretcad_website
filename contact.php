97df0ca5ec15ee55c1c50cb9657dd3168f50b81d
amelia-sacon <amelia.constantin87@gmail.com>
2023-11-23 19:13:22 +0200

    changed contact page

--- contact.php
+++ contact.php
@@ -26,48 +26,29 @@ include("includes/header.php");
 </div>
 </div>
 <br><br>
+<div class="row container">
+	<div class="col-12>
 <div id="container">
 <div id="preamble">
 <p>
 <span> Punct de lucru TIMISOARA: </span> <br>
-<iframe width="100%" height="300" frameborder="1" scrolling="no" marginheight="0" marginwidth="0" allowfullscreen 
-src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d5566.091581303304!2d21.23254624323249!3d45.77027255685748!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e0!4m0!4m3!3m2!1d45.7722115!2d21.2342709!5e0!3m2!1sde!2sro!4v1439995207936">
-</iframe><br /><small>
-<a href="https://goo.gl/maps/9rfM2" target="_blank" style="color:#0000FF;text-align:left">Vizualizare harta marita</a></small>
-	<br>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d291.7451721908237!2d21.217667931252574!3d45.77911295888879!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sro!4v1700758945488!5m2!1sen!2sro" width="100%" height="300" frameborder="1" scrolling="no" marginheight="0" marginwidth="0" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<br />
 	<table width="100%" >
 	<tr><td style="padding-left: 100px;">
 	
-	Strada Pomiculturii nr. 17 (colt cu Str. Constantin Cel Mare),
+	Strada Grigore Alexandrescu, nr. 176
 	</br>
 	Timisoara, jud. Timis <br>
-	<span> Telefon/Fax: </span> +40256-212124 <br>
+	<span> Telefon: </span> +4(0748)434039 <br>
 	<span> E-mail: </span> <a href="mailto:office@concretcad.ro">office@concretcad.ro</a>
 	</td></tr></table>
 	</p>
 	
 </div>
-</div>
-<br><br>
-<div id="container">
-<div id="preamble">	
-	<p>
-	<span> Punct de lucru SANNICOLAU MARE: </span> <br>
-	<iframe width="100%" height="300" frameborder="1" scrolling="no" marginheight="0" marginwidth="0" 
-	src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d2327.597855614853!2d20.622919051687326!3d46.071122311036966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e2!4m0!4m3!3m2!1d46.071025999999996!2d20.623178!5e0!3m2!1sro!2sro!4v1439995917995">
-	</iframe><br /><small>
-	<a href="https://goo.gl/maps/F9ajF" target="_blank" style="color:#0000FF;text-align:left">
-	Vizualizare harta marita</a></small>
-	<br>
-	<table width="100%" >
-	<tr><td style="padding-left: 100px;">
-	
-	Piata 30 Decembrie, nr. 12 (langa policlinica) </br>
-	Sannicolau Mare, jud. Timis </br>
-	<span> Telefon/Fax: </span> +40356-268483 <br>
-	<span> E-mail: </span> <a href="mailto:office@concretcad.ro">office@concretcad.ro</a> 
-	</td></tr></table>
-	</p>
+</div></div></div>
+<br>
 	
 <?php
 include("includes/footer.php");
